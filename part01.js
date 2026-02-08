fc487822bb02c20c3fc6c97e853ae73529650e54
Ranbir <ranbir.webdev@gmail.com>
2026-02-08 10:47:27 +0600

    first commit

---
+++ part01.js
@@ -0,0 +1,20 @@
+// problem_01:
+const BirthYear = 2005;
+
+// problem_02
+const brothers = 3;
+
+// problem_03
+const students = 42;
+
+// problem_04
+const marks = 85;
+
+// problem_05
+const rooms = 5;
+
+// problem_06
+const hours = 4;
+
+// problem_07
+const total = 11;
---
+++ part_02.js
@@ -0,0 +1,27 @@
+// problem_01
+const age = 22;
+console.log(age);
+
+// problem_02
+const booksToRead = 10;
+console.log(booksToRead);
+
+// problem_03
+const wakeUpTime = "7:00 am";
+console.log(wakeUpTime);
+
+// problem_04
+const wastedTime = 2.5;
+console.log(wastedTime);
+
+// problem_05
+const borrow = 500;
+console.log(borrow);
+
+// problem_06
+const goals = 5;
+console.log(goals);
+
+// problem_07
+const pillows = 2;
+console.log(pillows);
