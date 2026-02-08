f6265aa3cc1d0a0cc10f1f508c47b9e5487f56bf
Ranbir <ranbir.webdev@gmail.com>
2026-02-08 12:00:17 +0600

    add new practice problems part02 and 03

---
+++ part_03.js
@@ -0,0 +1,27 @@
+// problem_01
+const name = "Ranbir";
+console.log(name);
+
+// problem_02
+const address = "Dinajpur";
+console.log(address);
+
+// problem_03
+const collegeFriend = "Rahim";
+console.log(collegeFriend);
+
+// problem_04
+const fan = "Rasmika Mandana";
+console.log(fan);
+
+// problem_05
+const movieOfTheYear = "The Batman";
+console.log(movieOfTheYear);
+
+// problem_06
+const hobby = "playing football";
+console.log(hobby);
+
+// problem_07
+const color = "blue";
+console.log(color);
---
+++ part_04.js
@@ -0,0 +1,42 @@
+// problem_01
+const isHappy = true;
+const isSad = false;
+console.log(isHappy);
+console.log(isSad);
+
+// problem_02
+const crushSendsMessage = true;
+const crushIgnoresMessage = false;
+console.log(crushSendsMessage);
+console.log(crushIgnoresMessage);
+
+// problem_03
+const celebrating = true;
+console.log(celebrating);
+
+// problem_04
+const isVeryHappy = true;
+const hasMoney = false;
+const isHungry = true;
+
+console.log(isVeryHappy);
+console.log(hasMoney);
+console.log(isHungry);
+
+// problem_05
+const isStudyDone = true;
+console.log(isStudyDone);
+
+// problem_06
+const isPhoneCharged = false;
+console.log(isPhoneCharged);
+
+
+// problem_07
+const isDinnerDone = false;
+if (isDinnerDone){
+    console.log("Dinner is done");
+}
+else {
+    console.log("Dinner is not done");
+}
